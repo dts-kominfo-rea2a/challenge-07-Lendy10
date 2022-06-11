7e362b2c1f2d74b3721d1323e68eb0f16517952d
Lendy10 <lendy.pradhana.hartono-2016@fst.unair.ac.id>
2022-06-11 18:33:29 +0700

    finalisasi challange 7

--- promise.js
+++ promise.js
@@ -1,7 +1,25 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (keyword) => {
+  try {
+    //async kedua promise
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    //spread kedua data
+    const data = await [...theaterIXX, ...theaterVGC];
+
+    //hitung jumlah elemen yg sesuai keyword
+    const countMatchedKey = data.filter(item => {
+      return item.hasil == keyword
+    }).length;
+
+    return countMatchedKey
+  } catch (err) {
+    console.error(err)
+  }
+};
 
 module.exports = {
   promiseOutput,
